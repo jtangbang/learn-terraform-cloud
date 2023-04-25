1a52f1bb3ff73dde560321fabedda13d397e984a
jtangbang <131813542+jtangbang@users.noreply.github.com>
2023-04-25 13:40:30 -0700

    Create main.tf
    create Ec2 instance in AWS using teraform cloud

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "ap-south-1"
+}
+
+resource "aws_instance" "Demo" {
+  ami           = "ami-062df10d14676e201"
+  instance_type = "t2.micro"
+  key_name = "jtangbang"
+  tags = {
+    Name = "Demo"
+  }
+}

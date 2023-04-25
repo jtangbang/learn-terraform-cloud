c436405a2f77e90b249287686719a432e05c6637
jtangbang <131813542+jtangbang@users.noreply.github.com>
2023-04-25 13:56:44 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "Demo" {
-  ami           = "ami-062df10d14676e201"
+  ami           = "ami-014d05e6b24240371"
   instance_type = "t2.micro"
   key_name = "jtangbang"
   tags = {
